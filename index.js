33c5b16195e50fab639518846fb2fff7d38ee80b
Kosinskiy Andrey <kosinskiy.andrey@ukr.net>
2020-06-23 09:18:25 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+$( function() {
+    $( ".board" ).sortable({
+      connectWith: ".board",
+      placeholder: "placeholder-list",
+      forceHelperSize: true,
+      forcePlaceholderSize: true,
+      revert: true
+    }).disableSelection();
+
+    $( "list" ).draggable({
+        connectToSortable: ".board",
+        revert: "invalid"
+      });
+
+    $( ".portlet" )
+      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+      .find( ".portlet-header" )
+        .addClass( "ui-widget-header ui-corner-all" )
+        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+ 
+    $( ".portlet-toggle" ).on( "click", function() {
+      var icon = $( this );
+      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    }).disableSelection();
+  } );
