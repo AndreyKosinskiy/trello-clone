dbd703895cda8535172330376d33740e5c0668d2
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-06-23 14:44:18 +0300

    add sort on pointer intersect https://stackoverflow.com/questions/10637095/jquery-ui-sortable-tolerance-option-not-working-as-expected/10699339

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 $( function() {
     $( ".board" ).sortable({
-      connectWith: ".board",
+      // connectWith: ".board",
       placeholder: "placeholder-list",
       forceHelperSize: true,
       forcePlaceholderSize: true,
-      revert: true
+      revert: true,
+      tolerance: "pointer",
+      sort: function (event, ui) {
+        var that = $(this),
+            w = ui.helper.outerWidth();
+        that.children().each(function () {
+            if ($(this).hasClass('ui-sortable-helper') || $(this).hasClass('ui-sortable-placeholder')) 
+                return true;
+            // If overlap is more than half of the dragged item
+            var dist = Math.abs(ui.position.left - $(this).position().left),
+                before = ui.position.left > $(this).position().left;
+            if ((w - dist) > (w / 2) && (dist < w)) {
+                if (before)
+                    $('.ui-sortable-placeholder', that).insertBefore($(this));
+                else
+                    $('.ui-sortable-placeholder', that).insertAfter($(this));
+                return false;
+            }
+        });
+    },
     }).disableSelection();
 
-    $( "list" ).draggable({
-        connectToSortable: ".board",
-        revert: "invalid"
-      });
-
-    $( ".portlet" )
-      .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-      .find( ".portlet-header" )
-        .addClass( "ui-widget-header ui-corner-all" )
-        .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
- 
-    $( ".portlet-toggle" ).on( "click", function() {
-      var icon = $( this );
-      icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-      icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+    $( ".list-cards" ).sortable({
+      // connectWith: ".board",
+      placeholder: "placeholder-card",
+      forceHelperSize: true,
+      forcePlaceholderSize: true,
+      revert: true,
+      refreshPositions:true
     }).disableSelection();
-  } );
+
+    $( ".list-card" ).droppable({
+      accept: ".list-cards",
+      greedy: true
+    });
+    
+
+
+  //   $( ".list-wrapper" ).draggable({
+  //       connectToSortable: ".board",
+  //       helper:'original',
+  //       cursor: "pointer",
+  //       refreshPositions: true,
+  //     }).disableSelection();
+
+  });
